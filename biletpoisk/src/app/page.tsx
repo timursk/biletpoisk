c04c58ec2209d5f20f958703ac6692d886ca9285
Timur Salikhov <timur.dev@mail.ru>
2023-06-24 03:02:03 +0300

    feat: update main page, add styles, add BoxWrapper to TicketCard component

--- biletpoisk/src/app/page.tsx
+++ biletpoisk/src/app/page.tsx
@@ -1,26 +1,31 @@
-import { AcceptBtn } from '@/components/AcceptBtn/AcceptBtn';
-import { Accordion } from '@/components/Accordion/Accordion';
-import { CountBtn } from '@/components/CountBtn/CountBtn';
+import { BoxWrapper } from '@/components/BoxWrapper/BoxWrapper';
 import { FilterInput } from '@/components/FilterInput/FilterInput';
-import { FilterItem } from '@/components/FilterItem/FilterItem';
-import { Modal } from '@/components/Modal/Modal';
 import { TicketCard } from '@/components/TicketCard/TicketCard';
+import styles from './page.module.css';
 
 export default function Home() {
     return (
-        <div>
-            test
-            <FilterItem />
-            <FilterInput />
-            {/* <CountBtn isPlus={true} callback={() => {}} /> */}
-            {/* <CountBtn isPlus={false} callback={() => {}} /> */}
-            <AcceptBtn isAccept={true} />
-            <AcceptBtn isAccept={false} />
-            <Accordion question={'some qiestion'} answer={'answer bla bla'} />
-            <Accordion question={'some qiestion'} answer={'answer bla bla'} />
-            <Accordion question={'some qiestion'} answer={'answer bla bla'} />
-            <TicketCard />
-            <Modal />
+        <div className={styles.container}>
+            <div className={styles.filterContainer}>
+                <BoxWrapper>
+                    <div className={styles.filterWrapper}>
+                        <h3 className={styles.filterContainerTitle}>Фильтр поиска</h3>
+
+                        <div className={styles.filters}>
+                            <FilterInput />
+                            <FilterInput />
+                            <FilterInput />
+                        </div>
+                    </div>
+                </BoxWrapper>
+            </div>
+
+            <div className={styles.ticketsContainer}>
+                <TicketCard />
+                <TicketCard />
+                <TicketCard />
+                <TicketCard />
+            </div>
         </div>
     );
 }
--- biletpoisk/src/components/TicketCard/TicketCard.tsx
+++ biletpoisk/src/components/TicketCard/TicketCard.tsx
@@ -5,46 +5,49 @@ import styles from './ticketCard.module.css';
 import Image from 'next/image';
 import { CountBtn } from '../CountBtn/CountBtn';
 import close from '../../assets/icons/close.svg';
+import { BoxWrapper } from '../BoxWrapper/BoxWrapper';
 
 export const TicketCard: FC = () => {
     const [count, setCount] = useState(0);
 
     return (
-        <div className={styles.container}>
-            <div className={styles.imgContainer}>
-                <Image src={''} alt={''} width={100} height={120} />
-            </div>
-
-            <div className={styles.main}>
-                <div className={styles.titleContainer}>
-                    <h3 className={styles.title}>Властелин колец: Братство кольца</h3>
-                    <span>Фэнтези</span>
+        <BoxWrapper>
+            <div className={styles.container}>
+                <div className={styles.imgContainer}>
+                    <Image src={''} alt={''} width={100} height={120} />
                 </div>
 
-                <div className={styles.counter}>
-                    <CountBtn
-                        isPlus={false}
-                        callback={() => {
-                            setCount((count) => count - 1);
-                        }}
-                        isDisabled={count === 0}
-                    />
-                    <span>{count}</span>
-                    <CountBtn
-                        isPlus={true}
-                        callback={() => {
-                            setCount((count) => count + 1);
-                        }}
-                        isDisabled={false}
-                    />
-                </div>
+                <div className={styles.main}>
+                    <div className={styles.titleContainer}>
+                        <h3 className={styles.title}>Властелин колец: Братство кольца</h3>
+                        <span>Фэнтези</span>
+                    </div>
 
-                {true && (
-                    <button className={styles.closeBtn}>
-                        <Image src={close} alt={'delete'} width={20} height={20} />
-                    </button>
-                )}
+                    <div className={styles.counter}>
+                        <CountBtn
+                            isPlus={false}
+                            callback={() => {
+                                setCount((count) => count - 1);
+                            }}
+                            isDisabled={count === 0}
+                        />
+                        <span>{count}</span>
+                        <CountBtn
+                            isPlus={true}
+                            callback={() => {
+                                setCount((count) => count + 1);
+                            }}
+                            isDisabled={false}
+                        />
+                    </div>
+
+                    {true && (
+                        <button className={styles.closeBtn}>
+                            <Image src={close} alt={'delete'} width={20} height={20} />
+                        </button>
+                    )}
+                </div>
             </div>
-        </div>
+        </BoxWrapper>
     );
 };
