d417a10bdeb0b51bd55cf9badebbb5859a7c5c1a
Timur Salikhov <timur.dev@mail.ru>
2023-06-25 01:07:37 +0300

    feat: add Comment component, update film page

--- biletpoisk/src/app/film/page.tsx
+++ biletpoisk/src/app/film/page.tsx
@@ -1,10 +1,14 @@
 import { FilmCard } from '@/components/FilmCard/FilmCard';
 import styles from './film.module.css';
+import { Comment } from '@/components/Comment/Comment';
 
 export default function Film() {
     return (
-        <div>
+        <div className={styles.container}>
             <FilmCard />
+            <Comment />
+            <Comment />
+            <Comment />
         </div>
     );
 }
---
+++ biletpoisk/src/components/Comment/Comment.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+import styles from './comment.module.css';
+import { BoxWrapper } from '../BoxWrapper/BoxWrapper';
+import Image from 'next/image';
+
+export const Comment: FC = () => {
+    return (
+        <BoxWrapper>
+            <div className={styles.container}>
+                <Image src={''} alt={''} width={100} height={100} />
+
+                <div>
+                    <div className={styles.userContentContainer}>
+                        <h4 className={styles.bold}>Роман</h4>
+                        <span>
+                            Оценка: <span className={styles.bold}>8</span>
+                        </span>
+                    </div>
+
+                    <p>
+                        По счастью мне довелось посмотреть фильм раньше, чем прочесть книгу. Это
+                        было около четырех лет назад, но тот момент я вспоминаю и по сей день. До
+                        него я не был фанатом Джона Толкина, как впрочем, и всего фентези в целом,
+                        однако стоило мне посмотреть первые десять минут фильма и оставшиеся
+                        пролетели на одном дыхании. Я словно погрузился в необычайный мир, где добро
+                        борется со злом, где зеленые рощи перемежаются с поросшими мхом статуями и
+                        древними развалинами, в мир, где пробираясь лесною тропой можно встретить
+                        остроухих неувядающих эльфов или мерзких орков – кому как повезет...
+                    </p>
+                </div>
+            </div>
+        </BoxWrapper>
+    );
+};
