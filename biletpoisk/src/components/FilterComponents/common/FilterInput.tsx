fb42b0a72534568dc7e4a0be7a89057d461141a5
Timur Salikhov <timur.dev@mail.ru>
2023-06-27 01:06:24 +0300

    fix: restoring input value

--- biletpoisk/src/components/FilterComponents/common/FilterInput.tsx
+++ biletpoisk/src/components/FilterComponents/common/FilterInput.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { ChangeEvent, FC, useCallback, useContext, useMemo } from 'react';
+import { ChangeEvent, FC, useCallback, useContext, useEffect, useMemo, useState } from 'react';
 import styles from './filterInput.module.css';
 import { FiltersContext } from './FiltersWrapper';
 import { useAppDispatch } from '@/store/store';
 import { filtersActions } from '@/store/features/filters';
 import { debounce } from '@/utils/helpers';
+import { useSelector } from 'react-redux';
+import { selectTitle } from '@/store/features/filters/selectors';
 
 interface Props {
     placeholder: string;
@@ -13,17 +15,27 @@ interface Props {
 }
 
 export const FilterInput: FC<Props> = ({ placeholder, id }) => {
+    const title = useSelector(selectTitle);
+    const [value, setValue] = useState(title);
     const { switchFilter } = useContext(FiltersContext);
     const dispatch = useAppDispatch();
 
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        setValue(value);
+    };
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    const handleChange = useCallback(
-        debounce((e: ChangeEvent<HTMLInputElement>) => {
-            const { value } = e.target;
+    const changeFilter = useCallback(
+        debounce((value: string) => {
             dispatch(filtersActions.setTitle(value));
         }, 200),
         []
     );
+
+    useEffect(() => {
+        changeFilter(value);
+    }, [value, changeFilter]);
 
     return (
         <input
@@ -31,6 +43,7 @@ export const FilterInput: FC<Props> = ({ placeholder, id }) => {
             placeholder={placeholder}
             onClick={() => switchFilter(id)}
             onChange={handleChange}
+            value={value}
         ></input>
     );
 };
